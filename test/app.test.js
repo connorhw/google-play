b3a3bb124afcf4c7318c67c6227c4607cbbd103b
Connor Wong <connorwong93@gmail.com>
2020-03-13 23:14:32 -0700

    manish corrections, added needed expect statements in order to test properly

--- test/app.test.js
+++ test/app.test.js
@@ -12,12 +12,23 @@ describe('Google Playstore', () => {
         return supertest(app)
           .get('/apps')
           .query({ sort: 'Rating' })
-          //.expect(200, '');
+          .expect(200)
+          .then(res => {
+              //console.log(res.body)
+              expect(res.body).to.be.an('array');
+              for(i=0; i<res.body.length; i++){
+                  if(i>0) {
+                   expect(res.body[i].Rating).to.be.least(res.body[i-1].Rating)
+                  }
+              }
+          })
     });
+    /*
     it('should sort apps', () => {
         return supertest(app)
           .get('/apps')
           .query({ sort: 'App' })
           //.expect(200, '');
     });
+    */
   });
